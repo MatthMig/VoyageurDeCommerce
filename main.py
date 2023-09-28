a2e47e12ab78cb0c0c1774668f2c9a987cb0d2bc
Matthieu MIGNE <matthieu.migne@orange.fr>
2023-09-28 23:02:18 +0200

    First implementation of algorithm, not working infinite loop

--- main.py
+++ main.py
@@ -3,35 +3,69 @@ import queue
 
 
 class Node:
-    def __init__(self, parent, lower_bound, reduction_matrix, vertex=None, son_L=None, son_R=None):
+    def __init__(self, parent, lower_bound, matrix, vertex=None):
         self.parent = parent
         self.vertex = vertex
         self.lower_bound = lower_bound
-        self.reduction_matrix = reduction_matrix
-        self.son_L = son_L
-        self.son_R = son_R
+        self.matrix = matrix
+        self.son_L = None
+        self.son_R = None
 
-    def get_tour(self):
-        tour = [self.vertex]
-        node = self
-        while node.parent is not None:
-            node = node.parent
-            if node.vertex is not None:
-                tour.append(node.vertex)
-        return tour[::-1]
-    
+        if self.parent is not None:
+            if self.vertex is None:
+                self.parent.son_L = self
+            else:
+                self.parent.son_R = self
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.lower_bound == other.lower_bound
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, Node):
+            return self.lower_bound < other.lower_bound
+        raise TypeError("Comparison with unsupported type")
+
+    def __le__(self, other):
+        if isinstance(other, Node):
+            return self.lower_bound <= other.lower_bound
+        raise TypeError("Comparison with unsupported type")
+
+    def __gt__(self, other):
+        if isinstance(other, Node):
+            return self.lower_bound > other.lower_bound
+        raise TypeError("Comparison with unsupported type")
+
+    def __ge__(self, other):
+        if isinstance(other, Node):
+            return self.lower_bound >= other.lower_bound
+        raise TypeError("Comparison with unsupported type")
+
+# Returns reduction of the matrix and sum of reducing constants
 def reduction(matrix):
+    matrix = copy.deepcopy(matrix)
     h = 0
+    if [i for row in matrix for i in row if i != float('inf')] == []:
+        return matrix, float('inf')
     for i,r in enumerate(matrix):
+        if r.count(float('inf')) == len(r):
+            continue
         h += min(r)
         matrix[i] = [j-min(r) for j in r]
     for i in range(len(matrix[0])):
         column = [matrix[j][i] for j in range(len(matrix))]
+        if column.count(float('inf')) == len(column):
+            continue
         h += min(column)
         for j in range(len(matrix)):
             matrix[j][i] -= min(column)
     return matrix, h
 
+# Returns max regret and matrix position corresponding to it
 def regrets_calculation(matrix):
     max_regret = 0
     max_path = (0,0)
@@ -45,72 +79,71 @@ def regrets_calculation(matrix):
                 max_path = (i,j)
     return max_path[0], max_path[1], max_regret
 
-
 def little_algo(c):
     tree = queue.PriorityQueue()
-    reduction_matrix, h = reduction(copy.deepcopy(c))
+    reduction_matrix, h = reduction(c)
     i,j,regret = regrets_calculation(reduction_matrix)
-    tree.put((h, Node(parent=None, lower_bound=h, reduction_matrix=reduction_matrix)))
-    z0 = float('inf')
-    best_ending_node_tour = None
-    X = tree.get()[1]
-
-    while True:
-        k,l,regret = regrets_calculation(X.reduction_matrix)
-
-        # Left branch
-        X.son_L = Node(parent=X, lower_bound=X.lower_bound + regret, reduction_matrix=X.reduction_matrix)
-        Y_ = X.son_L
-        tree.put((Y_.lower_bound, Y_))
-
-        # Right branch
-        reduction_matrix.pop(k)
-        for row in reduction_matrix:
-            row.pop(l)
-        
-        m,p = X.get_path((k,l))
-        # Avoid subtours
-        # Get Starting point
-        node = X
-        while node.parent is not None:
-            node = node.parent
-            if node.vertex is not None:
-                p = node.vertex[0]
-        # Get Ending point
-        node = X
-        while node.son_R is not None:
-            node = node.son_R
-            if node.vertex is not None:
-                m = node.vertex[1]
-        reduction_matrix[m][p] = float('inf')
-
-        reduction_matrix, h = reduction(reduction_matrix)
-        i,j,regret = regrets_calculation(reduction_matrix)
-        X.son_R = Node(parent=X, lower_bound=X.lower_bound + h, reduction_matrix=reduction_matrix, vertex=(k,l))
-        Y = X.son_R
-        tree.put((Y.lower_bound, Y))
-
-        # Check if single tour node is near to complete it and update z0 and current ending node of best tour
-        if len(reduction_matrix) == len(reduction_matrix[0]) == 2:
-            if Y.lower_bound < z0:
-                z0 = Y.lower_bound
-                best_ending_node_tour = Y
-        
+    root = Node(parent=None, lower_bound=h, matrix=reduction_matrix)
+    tree.put((h, root))
+    absolute_lower_bound = h
+    while not tree.empty():
         X = tree.get()[1]
-        if node.lower_bound <= z0:
-            return best_ending_node_tour.get_tour()
-        if X == Y:
+        R, h = reduction(X.matrix)
+        if h == float('inf'):
             continue
-        cost_matrix = copy.deepcopy(c)
-        for i,vertex in enumerate(X.get_tour()):
-            cost_matrix[vertex[0]-i][vertex[1]-i] = 0
+        i, j, regret = regrets_calculation(R)
 
+        # Left branch (we do not take the path)
+        R_ = copy.deepcopy(R)
+        R_[i][j] = float('inf')
+        lower_bound = X.lower_bound + regret
+        if lower_bound <= absolute_lower_bound:
+            absolute_lower_bound = lower_bound
+        tree.put((lower_bound, Node(parent=X, lower_bound=lower_bound, matrix=R_)))
 
-matrix = [  [float('inf'), 27, 43, 16, 30, 26],
-            [7, float('inf'), 16, 1, 30, 25],
-            [20, 13, float('inf'), 35, 5, 0],
-            [21, 16, 25, float('inf'), 18, 18],
-            [12, 46, 27, 48, float('inf'), 5],
-            [23, 5, 5, 9, 5, float('inf')]]
+        # Right branch (we do take the path)
+        R[i] = [float('inf') for _ in range(len(R[i]))]
+        for row in R:
+            row[j] = float('inf')
+        R[j][i] = float('inf')
+        #lower_bound = X.lower_bound + h
+        lower_bound = float('inf')
+        if lower_bound <= absolute_lower_bound:
+            absolute_lower_bound = lower_bound
+        tree.put((lower_bound, Node(parent=X, lower_bound=lower_bound, matrix=R, vertex = (i,j))))
+    return root
 
-little_algo(matrix)
+matrix = [[float('inf'), 10, 15, 20],
+          [10, float('inf'), 35, 25],
+          [15, 35, float('inf'), 30],
+          [20, 25, 30, float('inf')]]
+
+root = little_algo(matrix)
+
+def find_min_leaf_with_parents(root):
+    min_leaf = None
+    parent_stack = []
+
+    def dfs(node):
+        nonlocal min_leaf
+        if not node:
+            return
+
+        parent_stack.append(node.vertex)
+
+        if not node.son_L and not node.son_R:
+            if min_leaf is None or node.lower_bound < min_leaf.lower_bound:
+                min_leaf = node
+        else:
+            if not node.son_L or (node.son_R and node.son_R.lower_bound < node.son_L.lower_bound):
+                dfs(node.son_R)
+            else:
+                dfs(node.son_L)
+
+        parent_stack.pop()
+
+    dfs(root)
+    
+    return parent_stack
+
+print(find_min_leaf_with_parents(root))
